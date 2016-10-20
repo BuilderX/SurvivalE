76d8e27d846e77e3403a7795ebe6bec746a1560e
Raymond Gonzalez <raymondgon7@gmail.com>
2016-10-20 15:49:53 -0400

    Update PlayerData.java

--- PlayerData.java
+++ PlayerData.java
@@ -5,18 +5,17 @@ import java.util.HashMap;
 public class PlayerData {
 	// TribeRelationship 
 	private HashMap<String,NPC> entity_Relationships;
-	private double staminaMax = 100D;
-	private double fatigueMin= 0D;
+	private final double staminaMax = 100D;
+	private final double fatigueMin= 0D;
 	private double stamina = 0D;
 	private double fatigue;
-	private Inventory Inventory;
 	private double exp;
+	private Inventory Inventory;
 	int waterAmount;
-	double currentBodyTemperature;
-	Disease sick;
 	int wellNessValue = 100;
 	int TribeMembers = 0;
-
+	double currentBodyTemperature;
+	Disease sick;
 
 	public PlayerData(HashMap<String, NPC> entity_Relationships, double staminaMax, double fatigueMin, double stamina, double fatigue, Calandar.Inventory inventory, double exp, int waterAmount, double currentBodyTemperature, Disease sick, int wellNessValue) {
 		this.entity_Relationships = entity_Relationships;
@@ -24,20 +23,29 @@ public class PlayerData {
 		this.fatigueMin = fatigueMin;
 		this.stamina = stamina;
 		this.fatigue = fatigue;
-		Inventory = inventory;
 		this.exp = exp;
 		this.waterAmount = waterAmount;
 		this.currentBodyTemperature = currentBodyTemperature;
 		this.sick = sick;
 		this.wellNessValue = wellNessValue;
+		Inventory = inventory;
 	}
 
 	public HashMap<String, NPC> getEntity_Relationshipss() {
 		return entity_Relationships;
 	}
-
+	public void addToMap(String s,NPC AI ){
+		if(s.length < 1 || AI == null){ 
+		 System.out.println('Invalid Input');	
+			return null;
+		}else{
+	          entity_Relationships.put(s,AI);
+		}
+	}
 	public void setEntity_Relationshipss(HashMap<String, NPC> entity_Relationshipss) {
-		this.entity_Relationships = entity_Relationshipss;
+		
+		if(entity_Relationshipss == null){ return null}
+		    this.entity_Relationships = entity_Relationshipss;
 	}
 
 	public double getStaminaMax() {
@@ -45,23 +53,22 @@ public class PlayerData {
 	}
 
 	public void setStaminaMax(double staminaMax) {
-		this.staminaMax = staminaMax;
+		if(staminaMax < 1){ return 0}
+		   this.staminaMax = staminaMax;
 	}
 
 	public double getFatigueMin() {
 		return fatigueMin;
 	}
 
-	public void setFatigueMin(double fatigueMin) {
-		this.fatigueMin = fatigueMin;
-	}
 
 	public double getStamina() {
 		return stamina;
 	}
 
 	public void setStamina(double stamina) {
-		this.stamina = stamina;
+		if(stamina < 1){ return 0}
+		   this.stamina = stamina;
 	}
 
 	public double getFatigue() {
@@ -69,7 +76,8 @@ public class PlayerData {
 	}
 
 	public void setFatigue(double fatigue) {
-		this.fatigue = fatigue;
+		if(fatigue < 1){ return 0}
+	           this.fatigue = fatigue;
 	}
 
 	public Calandar.Inventory getInventory() {
@@ -77,7 +85,8 @@ public class PlayerData {
 	}
 
 	public void setInventory(Calandar.Inventory inventory) {
-		Inventory = inventory;
+               if(inventory == null){ return null}
+		  Inventory = inventory;
 	}
 
 	public double getExp() {
@@ -85,7 +94,8 @@ public class PlayerData {
 	}
 
 	public void setExp(double exp) {
-		this.exp = exp;
+               if(exp < 1){ return 0}
+		   this.exp = exp;
 	}
 
 	public int getWaterAmount() {
@@ -93,7 +103,8 @@ public class PlayerData {
 	}
 
 	public void setWaterAmount(int waterAmount) {
-		this.waterAmount = waterAmount;
+                if(waterAmount < 1){ return 0}
+		    this.waterAmount = waterAmount;
 	}
 
 	public double getCurrentBodyTemperature() {
@@ -101,7 +112,8 @@ public class PlayerData {
 	}
 
 	public void setCurrentBodyTemperature(double currentBodyTemperature) {
-		this.currentBodyTemperature = currentBodyTemperature;
+	       if(currentBodyTemperature < 1){ return 0}
+		   this.currentBodyTemperature = currentBodyTemperature;
 	}
 
 	public Disease getSick() {
@@ -109,6 +121,7 @@ public class PlayerData {
 	}
 
 	public void setSick(Disease sick) {
+		if(fatigue === null){ return null}
 		this.sick = sick;
 	}
 
@@ -117,14 +130,17 @@ public class PlayerData {
 	}
 
 	public void setWellNessValue(int wellNessValue) {
+		if(wellNessValue < 1){ return 0}
 		this.wellNessValue = wellNessValue;
 	}
 
 	public PlayerData(int tribeMembers) {
+		if(tribeMembers < 1){ return 0}
 		TribeMembers = tribeMembers;
 	}
 
 	public int getTribeMembers() {
+		
 		return TribeMembers;
 	}
 }
