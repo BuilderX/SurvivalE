454d2b3f12e19eac1ca7b0eedf8f74e2a004b600
Raymond Gonzalez <raymondgon7@gmail.com>
2016-12-08 17:00:12 -0500

    Update LevelUpOption.java

--- LevelUpOption.java
+++ LevelUpOption.java
@@ -7,7 +7,7 @@ public abstract class LevelUpOption {
 
 
     private String name;
-    public String name() { return name; }
+    public  String name() { return name; }
 
     public LevelUpOption(String name){
         this.name = name;
