5c272d798d34229c99e5ae0757f6f3d46024538a
Raymond Gonzalez <raymondgon7@gmail.com>
2017-08-10 15:37:45 -0400

    Update Behaviors.java

--- Behaviors.java
+++ Behaviors.java
@@ -3,27 +3,66 @@ public class Behaviors{
      ANGRY,SAD,HAPPY,NEUTRAL,DEPRESSED,MAD,ILLUSIONS,HUNRGY,PARALYZED
     
     }
-     Animal a;
-    public Behaviors(Animal a ){
-       this.a = a;
+     AnimalData ani;
+    public Behaviors(AnimalData a){
+       this.ani = a;
     }
-    public void State(){
-         switch(B){
+    public void State(ani){
+         switch(ani.CurrentFeeling){
             case ANGRY:
+                 ani.attack += 5;
+                 ani.def -= 10;
+                 ani.lifeSpan -= 1;
+                 ani.healthiness -= 10;
+                 ani.angerLevel += 50;
+                 ani.hungerLv -= 5;
+                 ani.mentalHealth -= 10;
+              
             break;
             case HAPPY:
+                 ani.lifeSpan += 1;
+                 ani.healthiness += 10;
+                if(ani.healthiness > 200){
+                      ani.attack += 15;
+                 }
+                 ani.angerLevel -= 20;
+                 if(ani.angerLevel < 0){
+                      ani.angerLevel = 0;
+                 }
             break;
             case SAD:
-            break;
+                 ani.healthiness -= 10;
+                 ani.hungerLv += 10;
+                 if(ani.healthiness < 0 ){
+                      ani.Alive = false;
+                      initDeath();
+                 }
+                 int z = Math.round(Math.Random(0) * 1);
+             
+                 ani.angerLevel += 20;
+                 break;
             case DEPRESSED:
             break;
             case MAD;
             break;
-            case HUNGRY
+            case HUNGRY:
+               ani.hungerLv -= 10; 
+             if(ani.hungerLv > 110){
+                  ani.health -= ani.health;
+                 if(ani.health < 0){
+                  ani.alive = false;
+                  initDeath();
+                 }
+             }
             break;
             case ILLUSIONS:
+                 ani.mentalHealth -= 10;
+              
             break;
             CASE PARALYZED;
+                 ani.healthiness -= 10;
+              
+             
             break;
             default:
             break;
