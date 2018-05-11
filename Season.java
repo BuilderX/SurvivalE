eefc74a6af5643bd6af5d558e3af8e1a29d5bce1
Raymond Gonzalez <raymondgon7@gmail.com>
2018-05-11 15:33:52 -0400

    Update Season.java

--- Season.java
+++ Season.java
@@ -2,16 +2,12 @@ package Calandar;
 
 public enum Season {
 		
-		
-	
 		SPRING("Spring",16800,12000,1,1 ,0.6082D,0.01F,1850l, 0x00D900),
 		SUMMER("Spring",25000,5000,0,3 ,0.6082D,0.01F,1850l, 0x00D900),
 		FALL("Spring",12000,13000,3,2,0.6082D,0.01F,1850l, 0x00D900),
 		WINTER("Spring",11000,12500,2,1,0.6082D,0.01F,1850l, 0x00D900);
 		
 		//private final ResourceLocation resource;
-	
-		
 		private final int rain;
 		private final int thunder;
 		private final double factor;
@@ -20,10 +16,6 @@ public enum Season {
 		private final int seasonColor;
 		private final int startChance;
 		private final int endChance;
-		
-		
-		
-		 
 		 
 		private Season(String name,int startChance,int endChance, 
 				int rain,int seasonColor, 
@@ -46,13 +38,15 @@ public enum Season {
 		public int getEndChance() {
 			return endChance;
 		}
-
+			// amount
 		public int getRain() {
-			return rain;
+	               int ran = (int) Math.round(Math.random() * 10);
+		       return ran / rain;
 		}
 
 		public int getThunder() {
-			return thunder;
+			int ran = (int) Math.round(Math.random() * 10);
+			return thunder * ran;
 		}
 
 
