fbd13ed4afe07d6bc3c5584b8c4395d417feeec3
Raymond Gonzalez <raymondgon7@gmail.com>
2018-05-11 15:56:25 -0400

    Update Calendar.java

--- Calendar.java
+++ Calendar.java
@@ -4,7 +4,7 @@ public class Calendar {
 			public static int DAYS_PER_SEASON;
 			public static int TICKS_PER_DAY;
 			private static Configuration config;
-		
+	                private static int DayCount = 0;
 	     Calendar(){
 	            initCalender();
 	     }
@@ -13,11 +13,30 @@ public class Calendar {
 			TICKS_PER_DAY = getInteger("Ticks per day", 15000);
 	       }
 	       public void initCalender(){
-	                 
-	              
-	       
+	           initConfiguration(this.config);
+	           Season SPRING = Season.SPRING;
+		   Season SUMMER = Season.SUMMER;
+		   Season FALL = Season.FALL;
+		   Season WINTER = Season.WINTER;
+		       
+		   Season Seasons[] = {SPRING,SUMMER,FALL,WINTER};
+		       
+		   for(int i = 0; i < SEASONS.length;i++){  
+		     for(int j = 0; j < DAYS_PER_SEASON;j++){
+		        if(SEASONS[i].getStartChance() > 5000){
+			 if(SEASONS[i].getRain() > 5){
+		          if(SEASONS[i].getThunder() <= 111104){
+			    // Pass Results To Weather Generator
+			  }
+			 }
+		        }     
+		       for(int g = 0; g < TICKS_PER_DAY;g++){
+		       
+		       }	
+		         DayCount++;
+		     } 
+	           }
 	       }
-
 	       private static int getInteger(String string, int i) {
          	   return i;
 	       }
@@ -27,15 +46,11 @@ public class Calendar {
 	       private int getTicksPerSeason(){
 	           return TICKS_PER_DAY;
 	       }
-	
 	       private int setDayPerSeason(DPS){
 	           this.DAYS_PER_SEASON = DPS;
 	       }
 	       private int setTicksPerSeason(TPD){
 	           this.TICKS_PER_DAY = TPD;
 	       }
-	    
-	      
-	
 	
 }
